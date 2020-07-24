52b94006d51a55bdba3ea9c81d72ea9b5e8ef0c2
Denis Roystan <denisroystan01@gmail.com>
2020-07-24 21:29:24 +0530

    react setup

---
+++ site/precache-manifest.4beb5ccc4e8349dcde7b298beac65c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a179977680030e36e9ea692a0ed9e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02540088c16a03131c77",
+    "url": "/static/css/main.bc101738.chunk.css"
+  },
+  {
+    "revision": "30a8e322a93d1159ad95",
+    "url": "/static/js/2.a5ca8016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5ca8016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02540088c16a03131c77",
+    "url": "/static/js/main.b6fad14c.chunk.js"
+  },
+  {
+    "revision": "c4cda4214c0c300269f1",
+    "url": "/static/js/runtime-main.d5bfd598.js"
+  }
+]);
---
+++ web-app/src/components/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome Abroad!
+          One Page Personal Portfolio
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
