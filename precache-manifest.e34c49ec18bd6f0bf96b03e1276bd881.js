d02ba6ed9cf6bb5ef6d24fcba3a3551965ebea5a
Denis Roystan <denisroystan01@gmail.com>
2020-07-24 21:50:53 +0530

    Updates

---
+++ precache-manifest.e34c49ec18bd6f0bf96b03e1276bd881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe337ef42245488568bdc60642f7e55",
+    "url": "/persona/index.html"
+  },
+  {
+    "revision": "38427b4721689bce63cc",
+    "url": "/persona/static/css/main.bc101738.chunk.css"
+  },
+  {
+    "revision": "30a8e322a93d1159ad95",
+    "url": "/persona/static/js/2.a5ca8016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/persona/static/js/2.a5ca8016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38427b4721689bce63cc",
+    "url": "/persona/static/js/main.f03582b1.chunk.js"
+  },
+  {
+    "revision": "c0665db0ab4733f7f1f7",
+    "url": "/persona/static/js/runtime-main.df6c77cb.js"
+  }
+]);
